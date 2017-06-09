3e4d4e4a0c9469e58576983b82f76dfcb26529a0
Lucas Moreira <lmsouza@avenuecode.com>
2017-06-09 09:59:38 -0300

    First API steps to use routing

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,19 +8,75 @@ MyBlog.addRegions({
     mainRegion: '#main-region'
 });
 
-// MyBlog.Router =  Marionette.AppRouter.extend({
-//     appRoutes: {
-//         '': 'listPosts',
-//         ':post_name': 'listSingle'
-//     },
+var API = {
 
-//     listPosts: function() {
-//         console.log('list all posts');
-//     },
-//     listSingle: function(post_name) {
-//         console.log('list single post: ', post_name);
-//     }
-// });
+    listAllPosts: function() {
+        console.log('API: list all posts');
+
+        if ( !posts || posts == '' ) {
+
+            var fetchFromAPI = MyBlog.fetchPosts();
+            $.when( fetchFromAPI ).done(function(fetchFromAPI) {
+
+                // Instancio a collection de ItemViews
+                // A collection posts vem lá da promise()
+                var list = new MyBlog.listPosts({
+                    collection: posts
+                });
+
+                MyBlog.mainRegion.show( list );
+
+            });
+
+        }
+
+    },
+
+    listSinglePost: function(post_name) {
+        console.log('API: list single: ', post_name);
+
+        // var post_model = posts.get( post_name );
+        // console.log( post_model );
+
+        var singleModel = Backbone.Model.extend({});
+        var singleCollection = Backbone.Collection.extend({
+            model: singleModel
+        });
+
+        // ItemView do post completo
+        var fullItemView = Marionette.ItemView.extend({
+            template: '#single-post-full-template'
+        });
+        var collectionFullView = Marionette.CollectionView.extend({
+            childView: fullItemView
+        });
+
+        var theModel = new singleCollection([
+            new singleModel( post_model.attributes )
+        ]);
+        var fullPost = new collectionFullView({
+            collection: theModel
+        });
+
+        Backbone.history.navigate( this.model.id );
+
+        MyBlog.mainRegion.show( fullPost );
+    }
+}
+
+MyBlog.Router =  Marionette.AppRouter.extend({
+    appRoutes: {
+        '': 'listPosts',
+        ':post_name': 'listSingle'
+    },
+
+    listPosts: function() {
+        API.listAllPosts();
+    },
+    listSingle: function(post_name) {
+        API.listSinglePost(post_name);
+    }
+});
 
 // ItemView Loading
 MyBlog.loadingView = Marionette.ItemView.extend({
@@ -70,32 +126,12 @@ MyBlog.myItemView = Marionette.ItemView.extend({
         e.preventDefault();
         e.stopPropagation();
 
+        API.listSinglePost( this.model.id ); // post_name
+
         // console.log(this.model);
         // console.log(this.model.id);
 
-        var singleModel = Backbone.Model.extend({});
-        var singleCollection = Backbone.Collection.extend({
-            model: singleModel
-        });
-
-        // ItemView do post completo
-        var fullItemView = Marionette.ItemView.extend({
-            template: '#single-post-full-template'
-        });
-        var collectionFullView = Marionette.CollectionView.extend({
-            childView: fullItemView
-        });
-
-        var theModel = new singleCollection([
-            new singleModel(this.model.attributes)
-        ]);
-        var fullPost = new collectionFullView({
-            collection: theModel
-        });
-
-        Backbone.history.navigate( this.model.id );
-
-        MyBlog.mainRegion.show( fullPost );
+        
     }
 });
 
@@ -137,22 +173,10 @@ MyBlog.fetchPosts = function() {
 MyBlog.on('start', function() {
     if ( Backbone.history )
         Backbone.history.start();
-
-    if ( !posts || posts == '' ) {
-
-        var fetchFromAPI = MyBlog.fetchPosts();
-        $.when( fetchFromAPI ).done(function(fetchFromAPI) {
-
-            // Instancio a collection de ItemViews
-            // A collection posts vem lá da promise()
-            var list = new MyBlog.listPosts({
-                collection: posts
-            });
-
-            MyBlog.mainRegion.show( list );
-
-        });
-
-    }
+    
+    if ( Backbone.history.fragment == '' )
+        API.listAllPosts();
+    else
+        API.listSinglePost( Backbone.history.fragment );
 
 });
