116e539cc45a29699194c4d739ca5580747e92d6
Lucas Moreira <moreirapontocom@gmail.com>
2017-06-12 23:38:21 -0300

    Listing all posts

--- assets/js/router.js
+++ assets/js/router.js
@@ -19,13 +19,31 @@ var API = {
         var fetch = MyBlog.fetchPosts();
         $.when( fetch ).done(function() {
 
-            // O fetch retorna uma collection de models (Backbone.Collection)
+            // Defining ItemView
+            var myItemView = Marionette.ItemView.extend({
+                template: '#single-post-in-list-template',
+                tagName: 'li',
+                events: {
+                    'click .js-read-more': 'openSingle'
+                },
+                openSingle: function() {
+                    console.log('open: ', this.model);
+                }
+            });
 
-            var itens = new MyBlog.collectionViews({
+            // Defining CollectionView
+            var collectionViews = Marionette.CollectionView.extend({
+                tagName: 'ul',
+                className: 'post-list',
+                childView: myItemView
+            });
+
+            // Instancing CollectionView passing Collection of models (from fetch)
+            var views = new collectionViews({
                 collection: posts
             });
 
-            MyBlog.mainRegion.show( itens );
+            MyBlog.mainRegion.show( views );
 
         });
 
